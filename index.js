50bdde888da6f86b26413c22df9bcf2b7c37a811
JDGipson <jdgipson1989@gmail.com>
2023-06-27 10:52:53 -0500

    updating seed merge conflict

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Express server code goes here, routes, and middleware etc.
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+const client = require("../db/client");
+client.connect();
+
+app.use("api", require("/api"));
+
+app.listen(PORT, () => {
+  console.log(`The server is up and running on port: ${PORT}`);
+});
