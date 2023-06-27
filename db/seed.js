067eb553399b5dcf3bdcc6f00216373083ef01eb
Sarah Schultz <131220620+sarahschultz@users.noreply.github.com>
2023-06-27 10:47:52 -0500

    seed update from merge conflicts

--- db/seed.js
+++ db/seed.js
@@ -31,28 +31,6 @@ async function createTables() {
                 genre VARCHAR(255) NOT NULL,
                 msrp VARCHAR (255) NOT NULL,
                 score VARCHAR (255) NOT NULL,
-<<<<<<< HEAD
-                review TEXT DEFAULT 'no reviews',
-                author VARCHAR (255) NOT NULL,
-                comments TEXT DEFAULT 'no comments',
-                ourscore VARCHAR (255) NOT NULL,
-                picture TEXT NOT NULL,
-            )
-        
-        `);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function destroyTables() {
-  try {
-    await client.query(`
-            DROP TABLE IF EXISTS games;
-        `);
-  } catch (error) {
-    console.log(error);
-=======
                 ourreview TEXT DEFAULT 'no reviews',
                 studio VARCHAR (255) NOT NULL,
                 ourscore VARCHAR (255) NOT NULL,
@@ -83,23 +61,15 @@ async function destroyTables() {
     console.log("Finished creating tables");
   } catch (error) {
     throw error;
->>>>>>> sarahsbranch
   }
 }
 
 async function createNewGame(newGameObj) {
   try {
-<<<<<<< HEAD
-    const { rows } = await client.query
-    (`
-        INSERT INTO games(title, platform, genre, msrp, score, review, author, comments, ourscore, picture)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-=======
     const { rows } = await client.query(
       `
         INSERT INTO games(title, platform, genre, msrp, score, ourreview, studio, ourscore, picture)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
->>>>>>> sarahsbranch
         RETURNING *;
         `,
       [
@@ -108,14 +78,8 @@ async function createNewGame(newGameObj) {
         newGameObj.genre,
         newGameObj.msrp,
         newGameObj.score,
-<<<<<<< HEAD
-        newGameObj.review,
-        newGameObj.author,
-        newGameObj.comments,
-=======
         newGameObj.ourreview,
         newGameObj.studio,
->>>>>>> sarahsbranch
         newGameObj.ourscore,
         newGameObj.picture,
       ]
@@ -127,17 +91,6 @@ async function createNewGame(newGameObj) {
 }
 
 async function fetchAllGames() {
-<<<<<<< HEAD
-    try {
-        const { rows } = await client.query(`
-        SELECT * FROM games;
-        `);
-        
-        return rows;
-    } catch (error) {
-        console.log(error);
-    }
-=======
   try {
     const { rows } = await client.query(`
         SELECT * FROM games;
@@ -147,7 +100,6 @@ async function fetchAllGames() {
   } catch (error) {
     console.log(error);
   }
->>>>>>> sarahsbranch
 }
 
 async function fetchGameById(idValue) {
@@ -155,11 +107,7 @@ async function fetchGameById(idValue) {
     const { rows } = await client.query(`
         SELECT * FROM games
         WHERE "gameId" = ${idValue};
-<<<<<<< HEAD
-        `)
-=======
         `);
->>>>>>> sarahsbranch
 
     return rows[0];
   } catch (error) {
@@ -167,8 +115,6 @@ async function fetchGameById(idValue) {
   }
 }
 
-<<<<<<< HEAD
-=======
 // async function fetchGameByTitle(titleValue) {
 
 // }
@@ -178,52 +124,26 @@ async function fetchGameById(idValue) {
 // ETC.....
 
 // }
->>>>>>> sarahsbranch
 
 async function buildDatabase() {
   try {
     client.connect();
 
-<<<<<<< HEAD
-    await destroyTables();
-    await createTables();
-
-    const firstGame = await createNewGame ({
-=======
     await dropTables();
     await createTables();
 
     const gameDiablo4 = await createNewGame({
->>>>>>> sarahsbranch
       title: "Diablo 4",
       platform: "PC, XBox, PlayStation",
       genre: "Action RPG",
       msrp: "$69.99",
       score: "4",
-<<<<<<< HEAD
-      review:
-        "Diablo 4 is a game available on Battle.net. It costs almost $70 and will run higher for the ultimate collector's edition.",
-      author: "Blizzard",
-      comments:
-        "Diablo 4 is the 4th game released in the Diablo game franchise",
-      ourscore: "3",
-      picture: "url",
-    });
-    
-    const allGames = await fetchAllGames();
-    
-    const findSpecificGame = await fetchGameById(1);
-    // console.log(findSpecificGame);
-
-    client.end();
-
-=======
       ourreview:
         "Diablo 4 is a game available on Battle.net. It costs almost $70 and will run higher for the ultimate collector's edition.",
       studio: "Blizzard",
       ourscore: "3",
       picture: "url for Diablo 4 eventually",
-    })
+    });
 
     const gameTheLastOfUs = await createNewGame({
       title: "The Last of Us",
@@ -231,52 +151,48 @@ async function buildDatabase() {
       genre: "Action RPG",
       msrp: "$19.99",
       score: "5",
-      ourreview:
-        "The Last of Us is awesome! Sorry about her dad.",
+      ourreview: "The Last of Us is awesome! Sorry about her dad.",
       studio: "Naughty Dog",
       ourscore: "5",
       picture: "url for last of us eventually",
-    })
-    
+    });
+
     const gameTheLastOfUs2 = await createNewGame({
       title: "The Last of Us 2",
       platform: "PC, PlayStation 5",
       genre: "Action RPG",
       msrp: "$59.99",
       score: "5",
-      ourreview:
-        "The Last of Us 2 is better than the first.",
+      ourreview: "The Last of Us 2 is better than the first.",
       studio: "Naughty Dog",
       ourscore: "5",
       picture: "url for last of us 2 eventually",
-    })
-    
+    });
+
     const gameDyingLight = await createNewGame({
       title: "Dying Light",
       platform: "PlayStation 5",
       genre: "Survival",
       msrp: "$9.99",
       score: "5",
-      ourreview:
-        "The Last of Us is awesome! Sorry about her dad.",
+      ourreview: "The Last of Us is awesome! Sorry about her dad.",
       studio: "Techland",
       ourscore: "4",
       picture: "url for dying light eventually",
-    })
-    
+    });
+
     const gameDyingLight2 = await createNewGame({
       title: "Dying Light 2: Stay Human",
       platform: "PC, PlayStation 5",
       genre: "Survival",
       msrp: "$29.99",
       score: "4",
-      ourreview:
-        "The Last of Us is awesome! Sorry about her dad.",
+      ourreview: "The Last of Us is awesome! Sorry about her dad.",
       studio: "Techland",
       ourscore: "4",
       picture: "url for dying light 2 stay human eventually",
-    }) 
-    
+    });
+
     const gameHogwartsLegacy = await createNewGame({
       title: "Hogwarts Legacy",
       platform: "PC, PlayStation 5",
@@ -288,8 +204,8 @@ async function buildDatabase() {
       studio: "Avalance",
       ourscore: "5",
       picture: "url for Hogwarts Legacy eventually",
-    })
-    
+    });
+
     const gameDeadIsland2 = await createNewGame({
       title: "Dead Island 2",
       platform: "PC, PlayStation 5",
@@ -301,7 +217,7 @@ async function buildDatabase() {
       studio: "Deep Silver",
       ourscore: "3",
       picture: "url for dead island 2 eventually",
-    })
+    });
 
     const gameUncharted4 = await createNewGame({
       title: "Uncharted 4: A Thief's End",
@@ -309,12 +225,11 @@ async function buildDatabase() {
       genre: "Adventure RPG",
       msrp: "$24.99",
       score: "5",
-      ourreview:
-        "Uncharted 4, not Nathan Fillian.",
+      ourreview: "Uncharted 4, not Nathan Fillian.",
       studio: "Naughty Dog",
       ourscore: "5",
       picture: "url for uncharted 4 eventually",
-    })
+    });
 
     const gameUnchartedTheLostLegacy = await createNewGame({
       title: "Uncharted: The Lost Legacy",
@@ -322,12 +237,11 @@ async function buildDatabase() {
       genre: "Adventure RPG",
       msrp: "$39.99",
       score: "3",
-      ourreview:
-        "Was this the last of the uncharted series?.",
+      ourreview: "Was this the last of the uncharted series?.",
       studio: "Techland",
       ourscore: "4",
       picture: "url for Uncharted The Lost Legacy eventually",
-    })
+    });
 
     const gameRiseofTheTombRaider = await createNewGame({
       title: "Rise of the Tomb Raider",
@@ -335,12 +249,11 @@ async function buildDatabase() {
       genre: "Adventure RPG",
       msrp: "$29.99",
       score: "4",
-      ourreview:
-        "Was this the last of the uncharted series?.",
+      ourreview: "Was this the last of the uncharted series?.",
       studio: "Techland",
       ourscore: "5",
-      picture: "url for Rise of the Tomb Raider eventually", 
-    })
+      picture: "url for Rise of the Tomb Raider eventually",
+    });
 
     const gameDeadByDaylight = await createNewGame({
       title: "Dead by Daylight",
@@ -348,12 +261,11 @@ async function buildDatabase() {
       genre: "Adventure RPG, Horror",
       msrp: "$19.99",
       score: "4",
-      ourreview:
-        "This is one of the most fun to watch RP streams to.",
+      ourreview: "This is one of the most fun to watch RP streams to.",
       studio: "Behaviour Interactive Inc",
       ourscore: "4",
-      picture: "url for Dead by Daylight eventually", 
-    })
+      picture: "url for Dead by Daylight eventually",
+    });
 
     const gameCSGO = await createNewGame({
       title: "Counter-Strike: Global Offensive",
@@ -361,12 +273,11 @@ async function buildDatabase() {
       genre: "FPS",
       msrp: "$19.99",
       score: "4",
-      ourreview:
-        "Classic. Don't gamble lol.",
+      ourreview: "Classic. Don't gamble lol.",
       studio: "Behaviour Interactive Inc",
       ourscore: "4",
-      picture: "url for Counter-Strike: Global Offensive eventually", 
-    })
+      picture: "url for Counter-Strike: Global Offensive eventually",
+    });
 
     const gameStardewValley = await createNewGame({
       title: "Stardew Valley",
@@ -374,12 +285,11 @@ async function buildDatabase() {
       genre: "Simulation",
       msrp: "$14.99",
       score: "4",
-      ourreview:
-        "Basically a goat of sim games and farming sims.",
+      ourreview: "Basically a goat of sim games and farming sims.",
       studio: "Behaviour Interactive Inc",
       ourscore: "5",
-      picture: "url for stardew valley eventually", 
-    })    
+      picture: "url for stardew valley eventually",
+    });
 
     const gameCitiesSkylines = await createNewGame({
       title: "Cities: Skylines",
@@ -391,9 +301,8 @@ async function buildDatabase() {
         "Over 500 hours of gameplay, and that's without the expansion packs! Base game has so much content and so much to offer.",
       studio: "Paradox Interaction",
       ourscore: "5",
-      picture: "url for Cities Skylines eventually", 
-    })    
-
+      picture: "url for Cities Skylines eventually",
+    });
 
     const gameGolfWithFriends = await createNewGame({
       title: "Golf with Friends",
@@ -401,15 +310,11 @@ async function buildDatabase() {
       genre: "Sports",
       msrp: "$4.94",
       score: "3",
-      ourreview:
-        "So fun to play in an altered state with friends!",
+      ourreview: "So fun to play in an altered state with friends!",
       studio: "Paradox Interaction",
       ourscore: "3",
-      picture: "url for golf with friends eventually", 
-    })    
-
-
-    ;
+      picture: "url for golf with friends eventually",
+    });
 
     const allGames = await fetchAllGames();
     const findSpecificGame = await fetchGameById(1);
@@ -417,25 +322,16 @@ async function buildDatabase() {
     console.log(findSpecificGame);
 
     client.end();
->>>>>>> sarahsbranch
   } catch (error) {
     console.log(error);
   }
 }
 
-<<<<<<< HEAD
-// buildDatabase();
-=======
 buildDatabase();
->>>>>>> sarahsbranch
 
 module.exports = {
   fetchAllGames,
   fetchGameById,
   createNewGame,
-<<<<<<< HEAD
-};
-=======
   buildDatabase,
 };
->>>>>>> sarahsbranch
