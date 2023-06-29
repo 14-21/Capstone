b9357fc9ca55e18b9b51ad6062f0762cda122ed6
Sarah Schultz <131220620+sarahschultz@users.noreply.github.com>
2023-06-29 11:03:28 -0500

    updated users and seedData.js files

--- db/seedData.js
+++ db/seedData.js
@@ -41,7 +41,9 @@ async function createTables() {
     await client.query(`
         CREATE TABLE users (
           "userId" SERIAL PRIMARY KEY,
-          name VARCHAR(255) NOT NULL,
+          username VARCHAR(255) UNIQUE NOT NULL,
+          fname VARCHAR(255) NOT NULL,
+          lname VARCHAR(255) NOT NULL,
           password VARCHAR(255) NOT NULL,
           email VARCHAR(255) UNIQUE NOT NULL,
           profilepic VARCHAR(255) NOT NULL,
@@ -163,31 +165,29 @@ async function fetchGameByStudio(studioValue) {
         WHERE "studio" = '${studioValue}';
         `);
 
-        console.log(rows);
-        console.log("This is the fetchgameby studio function")
-        return rows[0];
+    return rows[0];
   } catch (error) {
     console.log(error);
   }
 }
 
 //Start of Users table section lines 174 through 209
-async function createInitialUsers(seedUser) {
+async function createInitialUsers(userObj) {
   try {
     const { rows } = await client.query(
       `
         INSERT INTO users(username, fname, lname, password, email, profilepic, is_admin)
         VALUES ($1, $2, $3, $4, $5, $6, $7)
-        RETURNING *;
+        RETURNING username;
         `,
       [
-        seedUser.username,
-        seedUser.fname,
-        seedUser.lname,
-        seedUser.password,
-        seedUser.email,
-        seedUser.profilepic,
-        seedUser.is_admin,
+        userObj.username,
+        userObj.fname,
+        userObj.lname,
+        userObj.password,
+        userObj.email,
+        userObj.profilepic,
+        userObj.is_admin,
       ]
     );
     return rows[0];
@@ -477,6 +477,90 @@ async function buildDatabase() {
       picture: "url for Crusader Kings III eventually",
     });
 
+    const gameNioh2 = await createNewGame({
+      title: "Nioh 2",
+      platform: "PC, PS5, XBOX",
+      genre: "Action, RPG",
+      msrp: "$29.99",
+      score: "3",
+      ourreview: "Almost more fructrating than the first. Yokai infest the land and our protagonist defends it!!",
+      studio: "KOEI TECMO Games Co Ltd",
+      ourscore: "3",
+      picture: "url for Nioh 2 eventually",
+    });
+
+    const gameF123 = await createNewGame({
+      title: "F123",
+      platform: "PC, PS5, XBOX",
+      genre: "Sports, Racing",
+      msrp: "$69.99",
+      score: "5",
+      ourreview: "Formula 1 racing sim at its best!",
+      studio: "Electronic Arts",
+      ourscore: "5",
+      picture: "url for F123 eventually",
+    });
+
+    const gameMadden24 = await createNewGame({
+      title: "Madden NFL 24",
+      platform: "PC, PS5, XBOX",
+      genre: "Action, Sports",
+      msrp: "$69.99",
+      score: "5",
+      ourreview: "WOW, another football game that accounts for such a large percentage of EA's annual revenue",
+      studio: "Electronic Arts",
+      ourscore: "4",
+      picture: "url for Madden NFL 24 eventually",
+    });
+
+    const gameGreenHell = await createNewGame({
+      title: "Green Hell",
+      platform: "PC, PS5, XBOX",
+      genre: "Horror, Survival",
+      msrp: "$24.99",
+      score: "4",
+      ourreview: "Being stalked by a jaguar and building a shelter, all whilst avoiding malaria. Cool horror sim, first person view.",
+      studio: "Creepy Jar",
+      ourscore: "5",
+      picture: "url for Green Hell eventually",
+    });
+
+    const gameMarioKart8 = await createNewGame({
+      title: "Mario Kart 8 Deluxe",
+      platform: "Nintendo Switch",
+      genre: "Adventure, Racing",
+      msrp: "$59.99",
+      score: "5",
+      ourreview: "Classic Mario Kart with modern graphics!",
+      studio: "Nintendo",
+      ourscore: "5",
+      picture: "url for Mario Kart 8 Deluxe eventually",
+    });
+
+    const gamePikmin4 = await createNewGame({
+      title: "Pikmin 4",
+      platform: "Nintendo Switch",
+      genre: "Action, RPG",
+      msrp: "$59.99",
+      score: "4",
+      ourreview: "Long awaited sequel!",
+      studio: "Nintendo",
+      ourscore: "4",
+      picture: "url for Pikmin 4 eventually",
+    });
+
+    const gameLegendOfZeldaTOK = await createNewGame({
+      title: "The Legend of Zelda: Tears of the Kingdom",
+      platform: "Nintendo Switch",
+      genre: "Action, RPG",
+      msrp: "$59.99",
+      score: "4",
+      ourreview: "Really cool graphics and really fun to play!",
+      studio: "Nintendo",
+      ourscore: "5",
+      picture: "url for The Legend of Zelda: Tears of the Kingdom eventually",
+    });
+
     const allGames = await fetchAllGames();
     const findSpecificGame = await fetchGameById(1);
     console.log(findSpecificGame);
@@ -726,7 +810,7 @@ async function buildDatabase() {
       username: "KTLarkin",
       fname: "Kody",
       lname: "Tern Larkin",
-      password: "HLDqN5vCF",
+      password: "HLDqN59vCF",
       email: "xisherwoodr@ask.com",
       profilepic: "url/href for KTLarkin",
       is_admin: false,
