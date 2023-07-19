4f210fc0cfb214fc1578dde960800a4246722e68
kgaz410 <131269259+kgaz410@users.noreply.github.com>
2023-07-19 14:16:01 -0400

    deploy changes

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const {Client} = require('pg')
 
-const client = new Client('postgress://localhost:5432/games')
 
-// const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/games';
 
-// const client = new Client({
-//   connectionString,
-//   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-// });
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/games';
+
+const client = new Client({
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 module.exports = client;
